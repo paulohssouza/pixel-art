484e549472f51726ea22ad4cc12f14db29972f86
Paulo Henrique <paulohss.souza@gmail.com>
2022-05-02 10:50:33 -0300

    Criação e alinhamento do grid de desenho.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const grid = document.getElementById('grid');
-const pixel = document.createElement('div');
-let size = 10;
+let size = 20;
+let contLine = 1;
+for (let index = 0; index < size; index += 1) {
+    const linePixel = document.createElement('div');
+    linePixel.id = 'line-grid' + (index + 1);
+    grid.appendChild(linePixel);
+    for (let index = 0; index < size; index += 1){
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        document.getElementById('line-grid' + contLine).appendChild(pixel);
+    }
+    contLine += 1;
+
+}
 
